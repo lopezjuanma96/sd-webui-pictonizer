14928fa7aa9b863cf7ae2874b7b0c582600221c2
Juanma López <lopezjuanma96@gmail.com>
2023-07-04 11:21:35 -0300

    differentiated full precision from normal

---
+++ sdwui-api-full.bat
@@ -0,0 +1,2 @@
+@echo off
+./webui.bat --listen --port=5421 --skip-version-check --skip-python-version-check --skip-torch-cuda-test --skip-prepare-environment --skip-install --no-half --precision=full --ckpt=models/Stable-diffusion/FullPictos274.safetensors
