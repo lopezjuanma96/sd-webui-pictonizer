d7eddc833da3c35a969bfb4149eeeac633922884
Juanma López <lopezjuanma96@gmail.com>
2023-07-24 15:04:21 -0300

    added upload function

--- README.md
+++ README.md
@@ -10,7 +10,8 @@ You can go to the original repo for more data on WebUI specifically, this are th
 - [x] Remove access to the original WebUI, to avoid breaking something, this will just be changing the start function on [](modules/launch_utils.py) script.
 - [ ] Add a middleware or extra request to improve - and maybe also translate - the user's prompt using an LLM model, for example using OpenAI GPT4 API.
 - [x] Add a postgeneration Picto Editor, to allow the user to edit the generated pictogram, with simple tools such as brush, eraser, color picker, etc. (like [this](https://github.com/JonSteinn/Web-Paint))
-- [ ] Complete postgeneration Editor with download and upload.
-- [ ] Add flood fill bucket to picto editor, maybe from [here](https://cantwell-tom.medium.com/flood-fill-and-line-tool-for-html-canvas-65e08e31aec6).
+- [x] Complete postgeneration Editor with upload.
+- [ ] Complete postgeneration Editor with download.
+- [x] Add flood fill bucket to picto editor, maybe from [here](https://cantwell-tom.medium.com/flood-fill-and-line-tool-for-html-canvas-65e08e31aec6).
 - [ ] Add img2img with edited (and maybe uploaded) picture.
 - [ ] Improve ngrok deployment, currently set up on `initialize` of `webui.py`, in which case should upgrade to paid version, or use a different tunneling service. 
--- public/pictonizer/js/index.js
+++ public/pictonizer/js/index.js
@@ -7,9 +7,14 @@ const editNav = document.getElementById('editNav');
 const outputText = document.getElementById('outputText');
 const outputImage = document.getElementById('outputImage');
 
+const promptUpload = document.getElementById('promptUpload');
+const editDownload = document.getElementById('editDownload');
+const uploaderInput = document.getElementById('uploaderInput');
+
 const getImageFromBase64 = (base64) => {
     const img = new Image();
-    img.src = 'data:image/png;base64,' + base64;
+    if (!base64.startsWith('data:image/png;base64,')) base64 = 'data:image/png;base64,' + base64;
+    img.src = base64;
     return img;
 }
 
@@ -39,11 +44,13 @@ const addImagesToOutput = (images) => {
     // a copy of the second so we skip it
     else if (images.length == 1) {
         addImageToOutput(images[0], 0, true);
-        return;
     }
-    for (let i = 1; i < images.length; i++) {
-        addImageToOutput(images[i], i);
+    else {
+        for (let i = 1; i < images.length; i++) {
+            addImageToOutput(images[i], i);
+        }
     }
+    addCanvasListeners();
 }
 
 const addImageToOutput = (image, index, only=false) => {
@@ -56,7 +63,7 @@ const addImageToOutput = (image, index, only=false) => {
     const thisOutputCanvasContext = thisOutputCanvas.getContext('2d', { willReadFrequently: true });
     outputImage.appendChild(thisOutputCanvas);
     const thisImage = getImageFromBase64(image);
-    thisImage.onload = () => thisOutputCanvasContext.drawImage(thisImage, 0, 0);
+    thisImage.onload = () => thisOutputCanvasContext.drawImage(thisImage, 0, 0, 512, 512);
 }
 
 const addNoImageToOutput = () => {
@@ -71,13 +78,15 @@ const addNoImageToOutput = () => {
 const showLoader = () => {
     promptSubmit.disabled = true;
     promptSubmit.value = "Loading...";
+    promptUpload.disabled = true;
     promptClear.disabled = true;
     outputImage.innerHTML = "<div class='loaderBlock'><div class='loader'></div></div>"
 }
 
 const hideLoader = () => {
     promptSubmit.disabled = false;
-    promptSubmit.value = "Submit";  
+    promptSubmit.value = "Submit";
+    promptUpload.disabled = false;  
     promptClear.disabled = false;
     outputImage.innerHTML = "";
 }
@@ -98,6 +107,8 @@ promptSubmit.addEventListener('click', () => {
         promptInput.setAttribute("invalid", true)
         return;
     }
+    hideEditNav();
+    showLoader();
     if (isDev) {
         console.log("dev mode image requested")
         showLoader();
@@ -107,7 +118,6 @@ promptSubmit.addEventListener('click', () => {
     // this later will be done in GPT4
     txt2imgBody.prompt = "a pictogram of " + inputValue + ", (vectorized, drawing, simplified, rounded face, digital art, icon)";
     txt2imgBody.negative_prompt = "(words, letters, text, numbers, symbols), (details, open traces, sharp corners, distorted proportion), (lip, nose, tooth, rouge, wrinkles, detailed face, deformed body, extra limbs)"
-    showLoader();
     fetch(txt2imgUrl, {
         method: 'POST',
         body: JSON.stringify(txt2imgBody),
@@ -160,6 +170,26 @@ promptInput.addEventListener('keypress', e => {
     if (e.key == "Enter") {
         e.preventDefault();
         promptSubmit.click();
+    }
+})
+
+promptUpload.addEventListener('click', () => {
+    uploaderInput.click();
+})
+
+uploaderInput.addEventListener('change', () => {
+    hideEditNav();
+    showLoader();
+    console.log("change")
+    const file = uploaderInput.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onloadend = () => {
+            hideLoader();
+            addImagesToOutput([reader.result])
+            uploaderInput.value = "";
+        }
     }
 })
 
